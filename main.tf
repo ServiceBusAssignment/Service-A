d33b727b338829ed05fd5684370c69cab31280e1
mleisa <melcafidan@gmail.com>
2024-01-25 10:08:03 +0100

    Update variables

--- main.tf
+++ main.tf
@@ -14,15 +14,18 @@ provider "azurerm" {
 
 module "service-bus-module" {
   source = "git::https://github.com/mleisa/assignment.git"
-
   namespace_name = "namespace-aa"
   queue_name_and_dlq = {
-    "queue1" = false
+    queue1 = {
+      queue_name             = "queue1"
+      dead_lettering_enabled = false
+    }
   }
   action_group_name = "team1"
+  action_group_short_name = "p1-action"
   alert_rule = {
     rule-a = {
-      alert_name         = "Servica-A-alert"
+      alert_name         = "Service-A-Queue1"
       metric_name        = "Messages"
       threshold          = 25
       dimension_operator = "Include"
